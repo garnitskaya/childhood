6aefb58620a62752b2008903e0382e987b8c8ed4
garnitskaya <82206556+garnitskaya@users.noreply.github.com>
2021-07-29 00:06:59 +0300

    Add files via upload

---
+++ category-soft_toys.php
@@ -0,0 +1,55 @@
+
+<?php
+    get_header();
+?>
+
+    <div class="toys">
+        <div class="container">
+            <h2 class="subtitle">Товары из категории "Мягкие игрушки"</h2>
+            <div class="toys__wrapper">
+
+            <?php
+                // параметры по умолчанию
+                $posts = get_posts( array(
+                    'numberposts' => -1, //что бы вывести все слайды
+                    'category_name'    => 'soft_toys',
+                    'orderby'     => 'date',
+                    'order'       => 'ASC',
+                    'post_type'   => 'post',
+                    'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+                ) );
+
+                foreach( $posts as $post ){
+                    setup_postdata($post);
+                    ?>
+
+                    <div class="toys__item" style="background-image: url(<?php 
+                        if(has_post_thumbnail()) { 
+                            the_post_thumbnail_url(); //устанавливаем привьюшку
+                        } else{
+                            echo get_template_directory_uri() . '/assets/img/not-found.jpg'; //если она не установлена будем подставлять эту
+                        }
+                    ?>)"> 
+                        <div class="toys__item-info">
+                            <div class="toys__item-title"><?php the_title(); ?></div>
+                            <div class="toys__item-descr">
+                                <?php the_field('toys_descr');?>
+                            </div>
+                            <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Подробнее</a>
+                        </div>
+                    </div>
+
+                <?php
+                    }
+
+                    wp_reset_postdata(); // сброс
+            ?>
+
+            </div>
+
+        </div>
+    </div>
+
+<?php
+    get_footer();
+?>
